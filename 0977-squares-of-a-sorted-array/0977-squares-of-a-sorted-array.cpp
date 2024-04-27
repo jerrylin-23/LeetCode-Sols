7eace77b85fc6d076b95c17f4480444471927839
Jerry Lin <108099648+jerrylin-23@users.noreply.github.com>
2024-04-27 17:30:17 -0400

    Update 0977-squares-of-a-sorted-array.cpp

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> Sorted(n);
+        int left = 0;
+        int right = nums.size() - 1;
+        int pos = n - 1;
+        while (left <= right) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                Sorted[pos] = nums[left] * nums[left];
+                pos--;
+                left++;
+            }
+            else {
+                Sorted[pos] = nums[right] * nums[right];
+                pos--;
+                right--;
+            }
+                
 
-        for (int x = 0; x < nums.size(); x++) {
-            nums[x] = nums[x] * nums[x];
+            
         }
-        sort(nums.begin(),nums.end() );
+        return Sorted;
 
-
-        return nums;
     }
 };
