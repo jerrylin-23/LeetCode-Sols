fe11205880edd1f2f21bab8c26446c23cb31286d
Jerry Lin <108099648+jerrylin-23@users.noreply.github.com>
2025-03-09 22:42:10 -0400

    Time: 0 ms (100%), Space: 19 MB (97.57%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,39 +1,21 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operators = {"+","*","-","/"}
         stack = []
+        operators = {"+", "-", "/", "*" }
 
         for char in tokens:
-            val1 = 0
-            val2 = 0
-            if char not in operators:
-                stack.append(int(char))
-            else:
+            if char in operators:
+                a = stack.pop()
+                b = stack.pop()
                 if char == "+":
-                    val1 = stack[-1]
-                    stack.pop()
-                    val2 = stack[-1]
-                    stack.pop()
-                    stack.append(val1 + val2)
-                elif char == "-":
-                    val1 = stack[-1]
-                    stack.pop()
-                    val2 = stack[-1]
-                    stack.pop()
-                    stack.append(val2 - val1)
-                elif char == "/":
-                    val1 = stack[-1]
-                    stack.pop()
-                    val2 = stack[-1]
-                    stack.pop()
-                    stack.append(int(val2 / val1))
-                elif char == "*":
-                    val1 = stack[-1]
-                    stack.pop()
-                    val2 = stack[-1]
-                    stack.pop()
-                    stack.append(val1 * val2)
-                
-        return stack[-1]
-
-                    
+                    stack.append(a + b)
+                if char == "-":
+                    stack.append(b - a)
+                if char == "/":
+                    stack.append(int(b / a))
+                if char == "*":
+                    stack.append(a * b)
+            else:
+                stack.append(int(char))
+        
+        return stack[0]
