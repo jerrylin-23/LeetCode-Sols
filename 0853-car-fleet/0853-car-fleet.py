1056f1bde8cf69beaa284662ddf2d7c8bf6fd0d3
Jerry Lin <108099648+jerrylin-23@users.noreply.github.com>
2025-03-15 17:22:12 -0400

    Time: 166 ms (57.22%), Space: 40.3 MB (26.24%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,15 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-
-        time = [(target - position[i]) / speed[i] for i in range(len(position))]
-
-        car =  sorted(zip(position, time), reverse= True, key=lambda x:x[0])
         stack = []
-        
+        cars = sorted(zip(position, speed), reverse = True)
 
-        for position, time in car:
+        times = [(target - pos) / spd for pos, spd in cars]
 
-            if not stack or time > stack[-1]:
+        for time in times:
+            if not stack or time > stack [-1]:
                 stack.append(time)
-        
         return len(stack)
