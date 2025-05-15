5f6848e948dcb8ebdc6e9ba2389b5c9a3cb5e41b
Jerry Lin <108099648+jerrylin-23@users.noreply.github.com>
2025-05-15 00:28:40 -0400

    Time: 2 ms (86.91%), Space: 18 MB (27.26%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,37 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        if not grid:
+            return -1 
+        rows, cols = len(grid) , len(grid[0])
+        time = 0
+        fresh_count = 0
         queue = deque()
-        fresh_count = 0 
 
-        for i in range (rows):
-            for j in range (cols):
-                if grid[i][j] == 1:
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r, c, 0)) 
+                if grid[r][c] == 1:
                     fresh_count += 1
-                elif grid[i][j] == 2:
-                    queue.append((i , j, 0))
 
-        
-        if fresh_count == 0:
-            return 0
-        
+                    
+              
+              
         while queue:
-
-            x , y , minute = queue.popleft()
-            curtime = minute
-
-            for dx, dy in [(-1 , 0 ), (1, 0), (0, -1 ), (0, 1)]:
-                nx = x + dx
-                ny = y + dy
-
+            row , col, cur_time = queue.popleft()
+            for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+                nx , ny = dx + row , dy + col
                 if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 1:
+                    queue.append((nx, ny, cur_time + 1))
                     grid[nx][ny] = 2
                     fresh_count -= 1
+                    time = cur_time + 1
 
-                    queue.append((nx , ny , minute + 1))
+        return -1 if fresh_count else time
+                
         
-        if fresh_count > 0:
-            return -1
-        
-        return curtime 
+                        
