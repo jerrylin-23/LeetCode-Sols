54da170c6ea1017552e4cae23c68429aac9a6271
Jerry Lin <108099648+jerrylin-23@users.noreply.github.com>
2025-05-23 00:30:30 -0400

    Time: 3 ms (89.5%), Space: 21.7 MB (74.88%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,41 +3,35 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board or not board[0]:
-            return 
-
-        m , n = len(board),  len(board[0])
-
-        def bfs (start):
-            queue = deque([start])
-            r, c  = start
-            board[r][c] = 'T'
-
-            while queue:
-                r, c =  queue.popleft()
-                for dx, dy in [(1 , 0), (-1, 0), (0, 1 ), (0 ,-1)]:
-                    nx , ny = dx + r, dy + c
-                    if 0 <= nx < m and 0 <= ny < n and board[nx][ny] == 'O':
-                        board[nx][ny] = 'T'
-                        queue.append((nx, ny))
-        for i in range(m):
-            if board[i][0] == 'O':
-                bfs((i, 0))
-            if board[i][n - 1] == 'O':
-                bfs((i, n - 1))
-        for j in range(n):
-            if board[0][j] == 'O':
-                bfs((0, j))
-            if board[m - 1][j] == 'O':
-                bfs((m - 1, j))
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'
-                elif board[i][j] == 'T':
-                    board[i][j] = 'O'
-
+        m = len(board)
+        n = len(board[0])
+        queue = deque()
+        
+        for r in range(m):
+            if board[r][0] == 'O':
+                queue.append((r,0))
+            if board[r][n-1] == 'O':
+                queue.append((r,n-1))
+        for c in range(n):
+            if board[0][c] == 'O':
+                queue.append((0,c)) 
+            if board[m-1][c] == 'O':
+                queue.append((m-1, c))
+           
         
 
+        while queue:
+            row, col = queue.popleft()
+            if 0 <= row < m and 0 <= col < n and board[row][col] == 'O':
+                board[row][col] = 'S'
+                for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx, ny = dx + row, dy + col
+                    queue.append((nx, ny))
 
-        
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                if board[r][c] == 'S':
+                    board[r][c] ='O'
